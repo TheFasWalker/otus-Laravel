35d4caa469e57ddbfacc83b104c3a78a5db63189
Vitalij <krinickij.v.a@gmail.com>
2025-11-12 21:23:16 +0300

    FEAT: edited registration page
    Доработана обертка для формы входа-регистрации
    Создано наполнение страницы для регистрации

--- resources/views/layouts/LoginLayOut.blade.php
+++ resources/views/layouts/LoginLayOut.blade.php
@@ -5,11 +5,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-        @vite(['resources/css/app.scss', 'resources/js/app.js'])
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 
     </head>
-    <body class="flex flex-col">
+    <body class="flex items-center justify-center w-screen h-screen relative">
+        <a
+        class="absolute top-0 left-0 ml-3 p-2 text-base font-medium text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group" 
+        href="/">на главную</a>
                 @yield("content")
     </body>
     </html>
