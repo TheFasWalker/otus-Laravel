d9d6b00e49521b4abf1dc7616f3906945a6efab1
Vitalij <krinickij.v.a@gmail.com>
2025-11-16 16:27:42 +0300

    REFACTOR: remover commented code

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,12 +16,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        // User::factory()->create([
-            // 'name' => 'Test User',
-            // 'email' => 'test@example.com',
-        // ]);
         $adminRoleId = DB::table('role')->where('name', 'admin')->value('id');
         $userRoleId = DB::table('role')->where('name','user')->value('id');
 
