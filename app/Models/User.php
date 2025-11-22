dadcc41ab1bddb35d6d6b01288a5db16f5c1c8fa
Vitalij <krinickij.v.a@gmail.com>
2025-11-22 16:25:20 +0300

    FEAT: aded roles table and relations roles-users

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,12 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    
+    public function role(){
+        return $this->belongsTo(Role::class);
+    }
+    
+    public function isAdmin():bool{
+        return $this->role_id==1;
+    }
 }
