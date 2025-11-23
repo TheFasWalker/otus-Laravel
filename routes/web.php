83d39f6f2a077a9f4107302f8c5f185c2dc27f1f
Vitalij <krinickij.v.a@gmail.com>
2025-11-23 13:51:21 +0300

    FEAT: aded base views and base routing

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,6 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -16,5 +13,19 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/', function(){
+    return view('pages.HomePage');
+});
+Route::get('/catalog',function(){
+    return view('pages.CatalogPage');
+})->name('catalog');
+Route::get('/product',function(){
+    return view('pages.ProductPage');
+})->name('product.page');
+Route::get('/lk',function(){
+    return view('pages.UserPage');
+})->name('lk');
+
 
 require __DIR__.'/auth.php';
