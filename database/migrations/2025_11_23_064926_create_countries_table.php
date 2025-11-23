8b984befca11625f5ca8656b7623ec81b661b58b
Vitalij <krinickij.v.a@gmail.com>
2025-11-23 09:54:15 +0300

    FEAT: aded mogration for country table

---
+++ database/migrations/2025_11_23_064926_create_countries_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+        DB::table('countries')->insert([
+            ['name'=>"RU"],
+            ['name'=>"RB"],
+            ['name'=>"UA"],
+            ['name'=>"KZ"]
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('countries');
+    }
+};
